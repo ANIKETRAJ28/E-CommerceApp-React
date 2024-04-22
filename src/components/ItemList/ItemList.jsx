899a60bc1fd814a4fbd3d1c890a9e9c4bb0d59ca
Aniket <aniketraj28042003@gmail.com>
2024-04-22 18:50:36 +0530

    added basic item and itemlist

---
+++ src/components/ItemList/ItemList.jsx
@@ -0,0 +1,23 @@
+import './ItemList.css';
+
+import Item from '../Item/Item';
+
+function ItemList({ shoppingItems }) {
+    return (
+        <div className="shopping-items-wrapper">
+            {
+                shoppingItems && shoppingItems.map(item => {
+                    return (
+                        <Item
+                            itemName={item.name}
+                            quantity={item.quantity}
+                            key={item.id}
+                        />
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default ItemList;
--- src/components/ShoppingList/ShoppingList.jsx
+++ src/components/ShoppingList/ShoppingList.jsx
@@ -2,6 +2,12 @@ import './ShoppingList.css';
 
 import ShopHeader from "../Header/ShopHeader";
 import InputItem from "../InputItem/InputItem";
+import ItemList from '../ItemList/ItemList';
+
+const shoppingItems = [
+    {id: 1, name: 'Apple', quantity: 2},
+    {id: 2, name: 'Rice', quantity: 3}
+]
 
 function ShoppingList() {
     return (
@@ -9,6 +15,7 @@ function ShoppingList() {
         <ShopHeader/>
         <div className="current-shopping-list">
             <InputItem/>
+            <ItemList shoppingItems={shoppingItems}/>
         </div> 
         </>
     )
