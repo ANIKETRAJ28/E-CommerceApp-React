0069a4f816d920a202bac9f0e3757dde8964cd71
Aniket <aniketraj28042003@gmail.com>
2024-04-22 19:25:32 +0530

    added ui changes for item list and added fa icons

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,6 +1,9 @@
 import './ItemList.css';
 
 import Item from '../Item/Item';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
+import { faMinus } from '@fortawesome/free-solid-svg-icons'
 
 function ItemList({ shoppingItems }) {
     return (
@@ -8,11 +11,14 @@ function ItemList({ shoppingItems }) {
             {
                 shoppingItems && shoppingItems.map(item => {
                     return (
-                        <Item
-                            itemName={item.name}
-                            quantity={item.quantity}
-                            key={item.id}
-                        />
+                        <div key={item.id} className='items-list'>
+                            <div className='change-quantity add-item'><FontAwesomeIcon icon={faPlus} /></div>
+                            <Item
+                                itemName={item.name}
+                                quantity={item.quantity}
+                            />
+                            <div className='change-quantity remove-item'><FontAwesomeIcon icon={faMinus} /></div>
+                        </div>
                     )
                 })
             }
