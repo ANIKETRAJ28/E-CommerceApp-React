9f2dba5a4209f173fd1922cc2df43b510466c332
Aniket <aniketraj28042003@gmail.com>
2024-04-25 13:47:56 +0530

    added react hook forms

--- src/components/InputItem/InputItem.jsx
+++ src/components/InputItem/InputItem.jsx
@@ -3,26 +3,33 @@ import './InputItem.css';
 import "react-toastify/dist/ReactToastify.css";
 
 // import { showSuccess } from '../../utils/showToast';
+import { useForm } from 'react-hook-form';
 
 function InputItem({ addItem }) {
-    const [itemName, setItemName] = useState('');
+    // const [itemName, setItemName] = useState('');
+    const {register, handleSubmit} = useForm();
+
+    const handleFormSubmit = (data) => {
+        // e.preventDefault();
+        // addItem(itemName);
+        // setItemName('');
+        addItem(data.item);
+    }
+
     return (
         <div className="item-input-wrapper">
-            <input
-                className="item-input"
-                type="text"
-                placeholder="Add an item..."
-                value={itemName}
-                onChange={(e) => setItemName(e.target.value)}
-            />
-            <button 
-                className='add-item-button' 
-                onClick={() => {
-                    addItem(itemName);
-                    setItemName('');
-                }} /*onClick={() => showSuccess("Successfully added item")}*/ >
-                Add
-            </button>
+            <form onSubmit={handleSubmit(handleFormSubmit)}>
+                <input
+                    className="item-input"
+                    type="text"
+                    placeholder="Add an item..."
+                    name='item'
+                    {...register('item', {required: true, minLength: 3})}
+                />
+                <button className='add-item-button' /*onClick={() => showSuccess("Successfully added item")}*/ >
+                    Add
+                </button>
+            </form>
         </div>
     )
 }   
